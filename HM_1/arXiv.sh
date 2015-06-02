2877495fd50d2ed5597cb54b9dcbc31c70e5c815
Juan Diaz <juantrix93@hotmail.com>
2015-06-02 12:55:28 -0500

    arxiv added

--- HM_1/arXiv.sh
+++ HM_1/arXiv.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+clear
 divider=============
 figlet arXiv
 printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="
 printf "%s\n" "Searching the arXiv for the new stuff"
 printf "%s\n" "http://arxiv.org/list/q-bio/new"
 printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="
-curl http://arxiv.org/list/q-bio/new>temp.sh
-grep Neurons temp.sh>num.sh
-#cat num.sh
-#printf "\n\n"
-grep Title: temp.sh>titulos.sh
-#cat titulos.sh
-#printf "\n\n"
+printf "keyword: $1\n"
+printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="
+
+curl -s http://arxiv.org/list/q-bio/new > temp.txt
+grep Title: temp.txt | grep -i $1 > titulos.txt
+printf "Articles found: %s\n" "$(wc -l titulos.txt | sed 's/titulos.txt//g')"
+sed 's/<span class="descriptor">Title:<\/span>/-/g' titulos.txt
+printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="
 
 
 
