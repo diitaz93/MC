7f7e9234179c541b4062cce1dc7663059ef82045
Juan Diaz <juantrix93@hotmail.com>
2015-06-02 14:28:49 -0500

    primer stardate

---
+++ HM_1/stardate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+divider==========
+printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="
+figlet StarDate
+printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="
+figlet $1
+
+printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="
+
+printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="
+
+
+
+
+
+
+
