ddd1c40f4538d107ad8eb81b9a824603ee367f0b
Juan Diaz <juantrix93@hotmail.com>
2015-06-03 12:09:46 -0500

    punto 1 con comentarios

--- HM_1/arXiv.sh
+++ HM_1/arXiv.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 clear
-divider=============
-figlet arXiv
-printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="
+divider=============	#Crea una constante de divisorque se va a imprimir constantemente
+figlet arXiv		#Muestra el texto 'bonito'
+printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="	#Imprime el divisor de la longitud deseada
 printf "%s\n" "Searching the arXiv for the new stuff"
 printf "%s\n" "http://arxiv.org/list/q-bio/new"
-printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="
-printf "keyword: $1\n"
-printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="
+printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="	#Imprime el divisor de la longitud deseada
+printf "keyword: $1\n"						#Imprime el valor que entra por parametro
+printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="	#Imprime el divisor de la longitud deseada
 
-curl -s http://arxiv.org/list/q-bio/new > temp.txt
-grep Title: temp.txt | grep -i $1 > titulos.txt
-printf "Articles found: %s\n" "$(wc -l titulos.txt | sed 's/titulos.txt//g')"
-sed 's/<span class="descriptor">Title:<\/span>/-/g' titulos.txt
-printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="
+curl -s http://arxiv.org/list/q-bio/new > temp.txt		#Importa el codigo fuente de la pg web
+grep Title: temp.txt | grep -i $1 > titulos.txt			#Redirige los titulos de los articulos que tienen la palabra clave
+printf "Articles found: %s\n" "$(wc -l titulos.txt | sed 's/titulos.txt//g')"	#Imprime el numero de articulos
+sed 's/<span class="descriptor">Title:<\/span>/-/g' titulos.txt	#Quita el texto que estorba
+printf "%s%s%s%s\n" "$divider" "$divider" "$divider" "="	#Imprime el divisor de la longitud deseada
 
 
 
