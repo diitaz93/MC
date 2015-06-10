a5e3c6bad62f5bacda1c1a13443d751b3b3c891f
Juan Diaz <juantrix93@hotmail.com>
2015-06-10 12:25:37 -0500

    adicionar archivos de ipynb

---
+++ python/exercices/ex10.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+a= raw_input("Ingrese un numero: ")
+b= raw_input("Ingrese otro numero: ")
+a=int(a)
+b=int(b)
+print("La suma de los numeros es: ")
+print a+b
+print("La diferencia de los numeros es: ")
+print b-a
+print("El producto de los numeros es: ")
+print b*a
